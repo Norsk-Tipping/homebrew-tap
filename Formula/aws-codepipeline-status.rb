c9e56ec74c00d7751ec1aca8232580d4e60989b7
roynesholen <126559066+roynesholen@users.noreply.github.com>
2026-07-03 11:37:24 +0200

    fix: korriger Ruby-interpolering i aws-codepipeline-status formula

--- Formula/aws-codepipeline-status.rb
+++ Formula/aws-codepipeline-status.rb
@@ -18,9 +18,9 @@ class AwsCodepipelineStatus < Formula
       resp = Net::HTTP.get(
         URI.parse("https://api.github.com/repos/Norsk-Tipping/los-common-utilities/releases/tags/#{tag}"),
         {
-"Accept" => "application/vnd.github+json",
-"Authorization" => "Bearer #{token}",
-"X-GitHub-Api-Version" => "2022-11-28"
+          "Accept" => "application/vnd.github+json",
+          "Authorization" => "Bearer #{token}",
+          "X-GitHub-Api-Version" => "2022-11-28"
         }
       )
 
@@ -29,10 +29,10 @@ class AwsCodepipelineStatus < Formula
     end
   end
 
-  url "\#{GitHubHelper.release_asset_url("aws-codepipeline-status/v0.1.0", "aws-codepipeline-status-0.1.0.tar.gz")}",
+  url "#{GitHubHelper.release_asset_url("aws-codepipeline-status/v0.1.0", "aws-codepipeline-status-0.1.0.tar.gz")}",
     headers: [
       "Accept: application/octet-stream",
-      "Authorization: Bearer \#{GitHubHelper.token}",
+      "Authorization: Bearer #{GitHubHelper.token}",
       "X-GitHub-Api-Version: 2022-11-28"
     ]
   sha256 "e69292e437acf1185482aa9671ef7eef524e96168b516e544528a2bf46e0dad6"
@@ -42,6 +42,6 @@ class AwsCodepipelineStatus < Formula
   end
 
   test do
-    system "\#{bin}/aws-codepipeline-status", "--help"
+    system "#{bin}/aws-codepipeline-status", "--help"
   end
 end
